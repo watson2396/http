bcb7352c002243b2acf6cb1702d743537868321b
Jacob Watson <JacobWatson@sbp.com>
2026-01-27 15:41:55 -0500

    update: working through ms socket examples

--- c/server.c
+++ c/server.c
@@ -16,35 +16,58 @@
 int main(int argc, char const* argv[])
 {
     printf("Hello, World\n");
-    int result;
+    int iResult;
 
     printf("initialising Winsock\n");
     WSADATA wsaData;
-    result = WSAStartup(MAKEWORD(2, 0), &wsaData);
-    if (result != 0) 
+    iResult = WSAStartup(MAKEWORD(2, 0), &wsaData);
+    if (iResult != 0) 
     {
-        printf("failed initialising Winsock, err: %d\n", result);
+        printf("failed initialising Winsock, err: %d\n", iResult);
         return 1;
     }
 
     //https://learn.microsoft.com/en-us/windows/win32/winsock/creating-a-socket-for-the-server
     struct addrinfo *result = NULL, *ptr = NULL, hints;
-    ZeroMemory(&hints, sizeof (hints));
+    ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_PASSIVE;result = NULL, *ptr = NULL, hints;
+    hints.ai_flags = AI_PASSIVE;
 
     // Resolve the local address and port to be used by the server
-    result = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-    if (result != 0) {
-        printf("getaddrinfo failed: %d\n", result);
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
         WSACleanup();
         return 1;
     }
 
-    // connect to socket
     SOCKET ListenSocket = INVALID_SOCKET;
+
+    // Create a SOCKET for the server to listen for client connections
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("Error at socket(): %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    // Setup the TCP listening socket
+    iResult = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(result);
+
+    //https://learn.microsoft.com/en-us/windows/win32/winsock/listening-on-a-socket
 
     // wait for incoming request on socket
 
